d6238ab7a13d63e9f152097f127cc034408a47d3
jessmorrison <jessmorrison90@gmail.com>
2019-09-30 20:37:12 -0500

    updated anim and line height

---
+++ build/precache-manifest.f205b4bb6839230d63f5cc210a077316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "722aa988e9f47d64ad211105da8ce7b7",
+    "url": "/portfolio/static/media/victory.722aa988.gif"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c605c51a26bdce63eb4d",
+    "url": "/portfolio/static/js/main.c605c51a.chunk.js"
+  },
+  {
+    "revision": "0fbacc4bbd1c597f2c6b",
+    "url": "/portfolio/static/js/1.0fbacc4b.chunk.js"
+  },
+  {
+    "revision": "c605c51a26bdce63eb4d",
+    "url": "/portfolio/static/css/main.b3f58ca9.chunk.css"
+  },
+  {
+    "revision": "c2bcb6b5e8dd146b97f559f1be2d1f31",
+    "url": "/portfolio/index.html"
+  }
+];
